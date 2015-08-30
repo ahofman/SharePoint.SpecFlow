fc9d733c160a8ba15e6495d05de1cc0bbcc3d7c3
Adran Hofman <adrian@adrianhofman.com>
2015-08-30 13:46:19 +1000

    Initial commit of source code.
    Added a single Given for GivenThereIsADocumentLibrary with a given name.
    Added a simple idempotency test.

---
+++ src/SharePoint.SpecFlow/DocumentLibraryGivens.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.SharePoint.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace SharePoint.SpecFlow
+{
+    [Binding]
+    public class DocumentLibraryGivens
+    {
+        public DocumentLibraryGivens()
+            : this(ScenarioContext.Current.GetWebUri())
+        {
+        }
+
+        public DocumentLibraryGivens(Uri currentWebUri)
+        {
+            _currentWebUri = currentWebUri;
+        }
+
+        [Given("there is a document library called \"(.*?)\"")]
+        public void GivenThereIsADocumentLibraryCalled(string docLibName)
+        {
+            var cc = new ClientContext(_currentWebUri);
+
+            List existingList = cc.Web.Lists.GetByTitle(docLibName);
+            cc.Load(existingList);
+
+            // I wonder if there's a better way of querying if a list exists without relying on exceptions?
+            try
+            {
+                cc.ExecuteQuery();
+            }
+            catch (ServerException)
+            {
+                existingList = null;
+            }
+
+            if (existingList != null)
+            {
+                // The list already exists.
+                // TODO: Make sure it's a document library...
+            }
+            else
+            {
+                var lci = new ListCreationInformation();
+                lci.Title = docLibName;
+                lci.TemplateType = (int)ListTemplateType.DocumentLibrary;
+                var addedList = cc.Web.Lists.Add(lci);
+                cc.Load(addedList);
+                cc.ExecuteQuery();
+            }
+        }
+
+        private Uri _currentWebUri;
+    }
+}
---
+++ src/SharePoint.SpecFlow/Extensions/ScenarioContextExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace SharePoint.SpecFlow
+{
+    public static class ScenarioContextExtensions
+    {
+        public static Uri GetWebUri(this ScenarioContext sc)
+        {
+            return sc["CurrentWebUri"] as Uri;
+        }
+
+        public static void SetWebUri(this ScenarioContext sc, Uri webUri)
+        {
+            sc["CurrentWebUri"] = webUri;
+        }
+    }
+}
